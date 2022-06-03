ff5d05d1b93bf6bbacdd2a0262c5ebee2bd84429
austinread <aread1800@gmail.com>
2022-06-03 16:11:50 -0400

    Accessibility refactor

--- WordWorldsXML/Directions.cs
+++ WordWorldsXML/Directions.cs
@@ -2,56 +2,56 @@ namespace WordWorldsXML;
 
 public class Directions 
 {
-    public static Direction North => new Direction
+    private static Direction North => new Direction
     {
         CompassDirection = 0,
         Name = "NORTH",
         ShortName = "N"
     };
-    public static Direction NorthEast => new Direction
+    private static Direction NorthEast => new Direction
     {
         CompassDirection = 1,
         Name = "NORTHEAST",
         ShortName = "NE"
     };
-    public static Direction East => new Direction
+    private static Direction East => new Direction
     {
         CompassDirection = 2,
         Name = "EAST",
         ShortName = "E"
     };
-    public static Direction SouthEast => new Direction
+    private static Direction SouthEast => new Direction
     {
         CompassDirection = 3,
         Name = "SOUTHEAST",
         ShortName = "SE"
     };
-    public static Direction South => new Direction
+    private static Direction South => new Direction
     {
         CompassDirection = 4,
         Name = "SOUTH",
         ShortName = "S"
     };
-    public static Direction SouthWest => new Direction
+    private static Direction SouthWest => new Direction
     {
         CompassDirection = 5,
         Name = "SOUTHWEST",
         ShortName = "S"
     };
-    public static Direction West => new Direction
+    private static Direction West => new Direction
     {
         CompassDirection = 6,
         Name = "WEST",
         ShortName = "W"
     };
-    public static Direction NorthWest => new Direction
+    private static Direction NorthWest => new Direction
     {
         CompassDirection = 7,
         Name = "NORTHWEST",
         ShortName = "NW"
     };
 
-    public static List<Direction> All => new List<Direction> { North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest };
+    private static List<Direction> All => new List<Direction> { North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest };
 
     public static bool Match(string input, out int direction)
     {
@@ -62,11 +62,11 @@ public class Directions
         
         return match != null;
     }
-}
 
-public class Direction
-{
-    public int CompassDirection;    //0-8
-    public string Name = "";
-    public string ShortName = "";
+    private class Direction
+    {
+        public int CompassDirection;    //0-8
+        public string Name = "";
+        public string ShortName = "";
+    }
 }
